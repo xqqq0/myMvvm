55f1bc4ebc6dc6180d6d37dd6e6dde34fe2ce207
程序小白猿 <15301072780@163.com>
2017-02-17 09:43:09 +0800

    项目初建

---
+++ mvvm+rac/mvvm+rac/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mvvm+rac
+//
+//  Created by 秦兴华 on 2017/2/17.
+//  Copyright © 2017年 秦兴华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
